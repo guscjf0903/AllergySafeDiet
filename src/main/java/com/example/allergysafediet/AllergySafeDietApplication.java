dba454efba9ccb8abd0a30d7a54097933dd5d8a2
hyunchuljung <guscjf0903@naver.com>
2024-02-22 21:37:55 +0900

    start

---
+++ src/main/java/com/example/allergysafediet/AllergySafeDietApplication.java
@@ -0,0 +1,13 @@
+package com.example.allergysafediet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergySafeDietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllergySafeDietApplication.class, args);
+    }
+
+}
