e87634af42ef52b867860b5b3e9ba9695262c878
hyunchuljung <guscjf0903@naver.com>
2024-02-23 17:54:29 +0900

    Feat : Add CunstomError
    원하는 에러 발생과 처리를 위한 CustomError, ExceptionHandler 기능 추가

---
+++ API/src/main/java/org/api/exception/CustomException.java
@@ -0,0 +1,10 @@
+package org.api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCodes errorCode;
+}
