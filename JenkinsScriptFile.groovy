151cc85759a368fabb1afe458bf7b4228dcbd736
hyunchuljung <guscjf0903@naver.com>
2024-04-16 22:20:31 +0900

    Fix : Fix prod jenkins
    젠킨스 스크립트 수정

--- JenkinsScriptFile.groovy
+++ JenkinsScriptFile.groovy
@@ -72,12 +72,8 @@ pipeline {
                 dir('API') {
                     script {
                         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS_S3_Credentials', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                            echo "Access key: $AWS_ACCESS_KEY_ID"
-                            echo "Secret key: $AWS_SECRET_ACCESS_KEY"
-
                             def dockerTag = "${repository}:API"
-
-                            dockerImageAPI = docker.build(dockerTag, "--build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY")
+                            dockerImageAPI = docker.build(dockerTag, "--build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY .")
                         }
                     }
                 }
