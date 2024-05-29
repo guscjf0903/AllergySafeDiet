4793aec9c76c4e613001c89c0eb579dfd6dcd3be
hyunchuljung <guscjf0903@naver.com>
2024-05-29 16:25:29 +0900

    Fix : Fix Aws
    EC2 주소 변경에 따른 수정

--- API/src/main/java/org/api/config/WebConfig.java
+++ API/src/main/java/org/api/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8888", "http://ec2-54-180-102-86.ap-northeast-2.compute.amazonaws.com:8888")
+                .allowedOrigins("http://localhost:8888", "http://ec2-3-34-179-33.ap-northeast-2.compute.amazonaws.com:8888")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- API/src/test/java/org/api/service/FoodRecordServiceTest.java
+++
@@ -1,7 +0,0 @@
-//package org.api.service;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class FoodRecordServiceTest {
-//
-//}
